375e6a8a17c52e71372fddf6b80e65f87b4440b1
ChangSik88 <abcdggh11@gmail.com>
2026-06-14 17:16:38 +0900

    feat:필요 라이브러리 파일명 수정

--- main.py
+++ main.py
@@ -164,4 +164,5 @@ async def get_all_policies():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
